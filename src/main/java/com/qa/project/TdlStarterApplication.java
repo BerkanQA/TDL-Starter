47efe8c17f33785eee7cab10983c7156541d4e2f
Your Name <you@example.com>
2021-03-19 15:40:43 +0000

    TDLWA-18 #comment Spring Boot project created

---
+++ src/main/java/com/qa/project/TdlStarterApplication.java
@@ -0,0 +1,13 @@
+package com.qa.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdlStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdlStarterApplication.class, args);
+	}
+
+}
